02bde7ff4ed3c3eace3a375ab1e1be27eb7b4b39
Adriaan Marain <adriaanmarain300@gmail.com>
2020-11-27 01:56:25 +0100

    wip

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const vision = require('@google-cloud/vision');
 const selectRandomFile = require('./helpers/select-random-file');
 const listenToUser = require('./listenToUser');
 
-const fs = require('fs');
-
-require('dotenv').config()
+require('dotenv').config();
 
 let lastEmbeddedVideoUrl = null;
 let democratMode = false;
@@ -18,13 +16,14 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    const [url] = msg.content.match(/((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?/) || [null];
+    const [url] = msg.content.match(
+        /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?/
+    ) || [null];
 
     lastEmbeddedVideoUrl = url || lastEmbeddedVideoUrl;
 
     if (msg.content.startsWith('w2g')) {
-        getWatchTogetherLink(lastEmbeddedVideoUrl)
-            .then(url => msg.reply(`Room created at: ${url}`));
+        getWatchTogetherLink(lastEmbeddedVideoUrl).then(url => msg.reply(`Room created at: ${url}`));
 
         return;
     }
@@ -40,11 +39,10 @@ client.on('message', msg => {
     }
 
     if (msg.content.toLowerCase().includes('inspir')) {
-        getQuote().then(({quote, url}) => {
+        getQuote().then(({ quote, url }) => {
             console.log(url);
             const embed = new Discord.MessageEmbed().setImage(url);
-            msg.channel
-                .send(quote, { tts: true, embed: embed});
+            msg.channel.send(quote, { tts: true, embed: embed });
         });
     }
 });
@@ -63,23 +61,23 @@ client.on('message', msg => {
 
 client.on('guildMemberSpeaking', (member, speaking) => {
     if (speaking.bitfield) {
-        listenToUser(member)
+        listenToUser(member);
         /* listenToMember(member) */
     }
 
     if (democratMode && speaking.bitfield > 0) {
-        playFromDir(member.voice.channel, './sounds/biden/')
+        playFromDir(member.voice.channel, './sounds/biden/');
     }
     if (republicanMode && speaking.bitfield > 0) {
-        playFromDir(member.voice.channel, './sounds/trump/')
+        playFromDir(member.voice.channel, './sounds/trump/');
     }
 });
 
 // client.on('typingStart', (channel, user) => {
-    // console.log(user.presence);
-    // if (user.presence.member.voice) {
-    //     playFromDir(user.presence.member.voice.channel, './sounds/typing/')
-    // }
+// console.log(user.presence);
+// if (user.presence.member.voice) {
+//     playFromDir(user.presence.member.voice.channel, './sounds/typing/')
+// }
 // });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
@@ -88,10 +86,10 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
         if (oldState.channel && oldState.channel.name === 'General') {
             playFromDir(oldState.channel, './sounds/disconnected/');
         }
-        
+
         // joined general
         if (newState.channel && newState.channel.name === 'General') {
-            playFromDir(newState.channel, './sounds/connected/');
+            listenToUser(newState.member);
         }
     }
 
@@ -99,12 +97,12 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if (oldState.selfDeaf && !newState.selfDeaf) {
         playFromDir(oldState.channel, './sounds/undeafened/');
     }
-})
+});
 
 client.login(process.env.DISCORD_TOKEN);
 
 async function getWatchTogetherLink(videoUrl = '') {
-    const response = await fetch("https://w2g.tv/rooms/create.json", {
+    const response = await fetch('https://w2g.tv/rooms/create.json', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -121,20 +119,22 @@ async function getWatchTogetherLink(videoUrl = '') {
 }
 
 async function getQuote() {
-    const response = await fetch("http://inspirobot.me/api?generate=true");
+    const response = await fetch('http://inspirobot.me/api?generate=true');
 
     const url = await response.text();
 
     const client = new vision.ImageAnnotatorClient();
-    client.api_key
+    client.api_key;
 
     const [result] = await client.textDetection(url);
-    let quote = result.textAnnotations[0]?.description || 'WATCH FOR MASSIVE BALLOT COUNTING ABUSE AND, JUST LIKE THE EARLY VACCINE, REMEMBER I TOLD YOU SO!';
-    quote = quote.replace(/(\r\n|\n|\r)/gm, " ").toLowerCase();
+    let quote =
+        result.textAnnotations[0]?.description ||
+        'WATCH FOR MASSIVE BALLOT COUNTING ABUSE AND, JUST LIKE THE EARLY VACCINE, REMEMBER I TOLD YOU SO!';
+    quote = quote.replace(/(\r\n|\n|\r)/gm, ' ').toLowerCase();
 
     console.log(quote);
 
-    return {quote, url};
+    return { quote, url };
 }
 
 function textToSpeech(text) {
@@ -144,7 +144,7 @@ function textToSpeech(text) {
 async function playFromDir(channel, dir) {
     const file = await selectRandomFile(dir);
 
-    return play(channel, dir+file);
+    return play(channel, dir + file);
 }
 
 async function play(channel, file) {
--- listenToUser.js
+++ listenToUser.js
@@ -1,16 +1,230 @@
-const fs = require("fs");
+const DeepSpeech = require('deepspeech');
+const Fs = require('fs');
+const Sox = require('sox-stream');
+const MemoryStream = require('memory-stream');
+const Duplex = require('stream').Duplex;
+const Wav = require('node-wav');
+
+/* const DEEPSPEECH_MODEL = __dirname + '/models/deepspeech-0.8.0-models';
+let SILENCE_THRESHOLD = 200; // how many milliseconds of inactivity before processing the audio
+
+// const VAD_MODE = VAD.Mode.NORMAL;
+// const VAD_MODE = VAD.Mode.LOW_BITRATE;
+// const VAD_MODE = VAD.Mode.AGGRESSIVE;
+const VAD_MODE = VAD.Mode.VERY_AGGRESSIVE;
+const vad = new VAD(VAD_MODE);
+
+function createModel(modelDir) {
+    let modelPath = modelDir + '.pbmm';
+    let scorerPath = modelDir + '.scorer';
+    let model = new DeepSpeech.Model(modelPath);
+    model.enableExternalScorer(scorerPath);
+    return model;
+}
+
+let englishModel = createModel(DEEPSPEECH_MODEL);
+
+let modelStream;
+let recordedChunks = 0;
+let silenceStart = null;
+let recordedAudioLength = 0;
+let endTimeout = null;
+let silenceBuffers = [];
+let firstChunkVoice = false;
+
+function processAudioStream(data, callback) {
+    vad.processAudio(data, 16000).then(res => {
+        if (firstChunkVoice) {
+            firstChunkVoice = false;
+            processVoice(data);
+            return;
+        }
+        switch (res) {
+            case VAD.Event.ERROR:
+                console.log('VAD ERROR');
+                break;
+            case VAD.Event.NOISE:
+                console.log('VAD NOISE');
+                break;
+            case VAD.Event.SILENCE:
+                processSilence(data, callback);
+                break;
+            case VAD.Event.VOICE:
+                processVoice(data);
+                break;
+            default:
+                console.log('default', res);
+        }
+    });
+
+    // timeout after 1s of inactivity
+    clearTimeout(endTimeout);
+    endTimeout = setTimeout(function () {
+        console.log('timeout');
+        resetAudioStream();
+    }, SILENCE_THRESHOLD * 3);
+}
+
+function endAudioStream(callback) {
+    console.log('[end]');
+    let results = intermediateDecode();
+    if (results) {
+        if (callback) {
+            callback(results);
+        }
+    }
+}
+
+function resetAudioStream() {
+    clearTimeout(endTimeout);
+    console.log('[reset]');
+    intermediateDecode(); // ignore results
+    recordedChunks = 0;
+    silenceStart = null;
+}
+
+function processSilence(data, callback) {
+    if (recordedChunks > 0) {
+        // recording is on
+        process.stdout.write('-'); // silence detected while recording
+
+        feedAudioContent(data);
+
+        if (silenceStart === null) {
+            silenceStart = new Date().getTime();
+        } else {
+            let now = new Date().getTime();
+            if (now - silenceStart > SILENCE_THRESHOLD) {
+                silenceStart = null;
+                console.log('[end]');
+                let results = intermediateDecode();
+                if (results) {
+                    if (callback) {
+                        callback(results);
+                    }
+                }
+            }
+        }
+    } else {
+        process.stdout.write('.'); // silence detected while not recording
+        bufferSilence(data);
+    }
+}
+
+function bufferSilence(data) {
+    // VAD has a tendency to cut the first bit of audio data from the start of a recording
+    // so keep a buffer of that first bit of audio and in addBufferedSilence() reattach it to the beginning of the recording
+    silenceBuffers.push(data);
+    if (silenceBuffers.length >= 3) {
+        silenceBuffers.shift();
+    }
+}
+
+function addBufferedSilence(data) {
+    let audioBuffer;
+    if (silenceBuffers.length) {
+        silenceBuffers.push(data);
+        let length = 0;
+        silenceBuffers.forEach(function (buf) {
+            length += buf.length;
+        });
+        audioBuffer = Buffer.concat(silenceBuffers, length);
+        silenceBuffers = [];
+    } else audioBuffer = data;
+    return audioBuffer;
+}
+
+function processVoice(data) {
+    silenceStart = null;
+    if (recordedChunks === 0) {
+        console.log('');
+        process.stdout.write('[start]'); // recording started
+    } else {
+        process.stdout.write('='); // still recording
+    }
+    recordedChunks++;
+
+    data = addBufferedSilence(data);
+    feedAudioContent(data);
+}
+
+function createStream() {
+    modelStream = englishModel.createStream();
+    recordedChunks = 0;
+    recordedAudioLength = 0;
+}
+
+function finishStream() {
+    if (modelStream) {
+        let start = new Date();
+        let text = modelStream.finishStream();
+        if (text) {
+            let recogTime = new Date().getTime() - start.getTime();
+            return {
+                text,
+                recogTime,
+                audioLength: Math.round(recordedAudioLength),
+            };
+        }
+    }
+    silenceBuffers = [];
+    modelStream = null;
+}
+
+function intermediateDecode() {
+    let results = finishStream();
+    createStream();
+    return results;
+}
+
+function feedAudioContent(chunk) {
+    recordedAudioLength += (chunk.length / 2) * (1 / 16000) * 1000;
+    modelStream.feedAudioContent(chunk);
+}
+
+function onRecognize(results) {
+    if (results.text === 'quit') {
+        console.log('quitting...');
+    } else {
+        console.log('recognized:', results);
+    }
+} */
+
+let modelPath = './models/deepspeech-0.8.0-models.pbmm';
+
+let model = new DeepSpeech.Model(modelPath);
+
+let desiredSampleRate = model.sampleRate();
+
+let scorerPath = './models/deepspeech-0.8.0-models.scorer';
+
+model.enableExternalScorer(scorerPath);
+
 
 
 module.exports = async function listenToUser(member) {
-    async function listenToMember(member) {
-        const connection = await member.voice.channel.join();
+    const connection = await member.voice.channel.join();
 
-        console.log(member)
+    /* createStream(); */
 
-        const audio = connection.receiver.createStream(member, { mode: 'pcm' });
-        
-        
+    const audioStream = connection.receiver.createStream(member, { mode: 'pcm' });
 
-        /* stream.on('close', () => console.log('stream closed')) */
-    }   
+    /* audioStream.on('data', function (data) {
+        processAudioStream(data, results => {
+            onRecognize(results);
+        });
+    }); */
+
+    audioStream.on('end', () => {
+        let audioBuffer = audioStream.toBuffer();
+    
+        const audioLength = (audioBuffer.length / 2) * (1 / desiredSampleRate);
+        console.log('audio length', audioLength);
+    
+        let result = model.stt(audioBuffer);
+    
+        console.log('result:', result);
+    });
+
+    /* stream.on('close', () => console.log('stream closed')) */
 };
